92819641aecaae56b9fe934cffeaa20abe8ca1a1
marie-josee <mariejosee89@hotmail.fr>
2024-07-31 17:39:24 +0200

    teste 1

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.getElementById('appelTexte').addEventListener('click', function() {
     button.innerHTML = '06 12 34 56 78';
     button.style.borderRadius = '5px'; // Pour que le bouton devienne rectangulaire une fois cliqué
 });
+
+
+// ********************************** le burger *****************************
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    burger.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show');
+        burger.classList.toggle('active');
+    });
+});
